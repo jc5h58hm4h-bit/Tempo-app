1546ce1119693b97bb821a4578bf46c3c9ebc3db
jc5h58hm4h-bit <304563573+jc5h58hm4h-bit@users.noreply.github.com>
2026-07-22 23:13:05 +0200

    utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -35,7 +35,9 @@ export function shuffleArray<T>(items: T[]): T[] {
   const result = [...items];
   for (let i = result.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]];
+    const temp = result[i] as T;
+    result[i] = result[j] as T;
+    result[j] = temp;
   }
   return result;
 }
